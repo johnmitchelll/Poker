f0d851a218cfd99d08a1941b03101f1be665356a
johnmitchelll <johnnymitchell24@icloud.com>
2023-10-26 10:27:19 -0700

    db service

--- database/index.js
+++ database/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
-console.log("HELLO");
+const uri = process.env.MONGODB_URI;
 
-mongoose.connect('mongodb+srv://COOper3211:COOper3211@cluster0.818iaj7.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp')
+mongoose.connect(process.env.MONGODB_URI ||
+                'mongodb+srv://COOper3211:COOper3211@cluster0.818iaj7.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp')
 .then(() => console.log("CONNECTED TO DB"))
 .catch((err) => console.log(err));
